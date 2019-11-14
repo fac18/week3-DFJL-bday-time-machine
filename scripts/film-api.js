7d4f806cb82d6edb8ea86a27c7d7fe302b10625a
Alexandra O'Mahony <alexandra.omahony1@gmail.com>
2019-11-14 12:26:40 +0000

    experimented heavily with import/export and dom manipulation - why does the forEach not work?!
    co-authored-by: Dan <dangoss@posteo.net>

--- scripts/film-api.js
+++ scripts/film-api.js
@@ -1,14 +1,15 @@
+// export let topFilms = [];
 (function() {
+  let topFilms = [];
+
   const xhr = new XMLHttpRequest();
   const birthday = document.querySelector(".input-date");
   const url = "https://api.themoviedb.org/3/discover/movie?";
   const api_key = "9ec25de3852c136e9679b83b4d5f1ad2";
   const year = 2019;
-
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       const filmObjs = JSON.parse(xhr.responseText).results;
-      let topFilms = [];
       filmObjs.forEach((x, i) => {
         if (i < 3) {
           let film = {
@@ -21,33 +22,6 @@
           topFilms.push(film);
         }
       });
-      console.log("topFilms: ", topFilms);
-      // filmOutput.innerHTML = xhr.responseText;
-      topFilms.forEach((x, i) => {
-        console.log(topFilms[0].title);
-        //create div for each object
-        // create image title, date and overview elements, for each div
-        var newDiv = document.createElement("div");
-        document.getElementById("film-output").appendChild(newDiv);
-        newDiv.className = "film number [i]";
-        console.log([i].image);
-        // const filmImage = document.querySelector("#film-image");
-        const filmTitle = document.createTextNode("");
-        filmTitle.textContent = topFilms[i].title;
-        const filmDate = document.createTextNode("");
-        filmDate.textContent = topFilms[i].release_date;
-        const filmOverview = document.createTextNode("");
-        filmOverview.textContent = topFilms[i].overview;
-
-        newDiv.appendChild(filmTitle);
-        newDiv.appendChild(filmDate);
-        newDiv.appendChild(filmOverview);
-
-        //
-        //
-
-        console.log(topFilms);
-      });
     }
   };
   xhr.open(
@@ -56,4 +30,35 @@
     true
   );
   xhr.send();
+
+  const filmsSection = document.querySelector(".films");
+  console.log(topFilms);
+
+  // const createFilmsFunction = function(filmArray) {
+  for (let i = 0; i < topFilms.length; i++) {
+    console.log(topFilms);
+    console.log("forEach running");
+    console.log(topFilms[i].title);
+    //create div for each object
+    // create image title, date and overview elements, for each div
+    let filmOutput = document.createElement("div").classList.add(`films__${i}`);
+    //
+    // console.log("image", filmObject[i].image);
+    // // const filmImage = document.createElement
+    // const filmTitle = document
+    //   .createElement("h3")
+    //   .classList.add("films__title");
+    // filmTitle.textContent = filmObject[i].title;
+    // const filmDate = document.createElement("p").classList.add("films__date");
+    // filmDate.textContent = filmObject[i].release_date;
+    // const filmOverview = document
+    //   .createElement("p")
+    //   .classList.add("films__overview");
+    // filmOverview.textContent = filmObject[i].overview;
+
+    filmsSection.appendChild(filmOutput);
+  }
+  // };
+
+  // createFilmsFunction(topFilms);
 })();
