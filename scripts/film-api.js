1ef4079a2d36a24633904d2c272e06801af5e78c
Alexandra O'Mahony <alexandra.omahony1@gmail.com>
2019-11-14 10:20:41 +0000

    adding foreach to filmapi to create nodes and elements
    co-authored-by: nikke <nikke.san@gmail.com>

--- scripts/film-api.js
+++ scripts/film-api.js
@@ -5,37 +5,55 @@
   const api_key = "9ec25de3852c136e9679b83b4d5f1ad2";
   const year = 2019;
 
-  const filmOutput = document.querySelector(".film-output");
-  const filmImage = document.querySelector(".film-image");
-  const filmTitle = document.querySelector(".film-title");
-  const filmDate = document.querySelector(".film-date");
-  const filmOverview = document.querySelector(".film-overview");
- 
-
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       const filmObjs = JSON.parse(xhr.responseText).results;
       let topFilms = [];
-        filmObjs.forEach((x, i) => {
-          if (i<3) {
-            let film = {
-              id: x.id,
-              image: x.poster_path,
-              title: x.title,
-              release_date: x.release_date,
-              overview: x.overview
-            }
-          topFilms.push(film))};
-          filmOutput.innerHTML = xhr.responseText;
-          filmImage.src = topFilms.film.image;
-          filmTitle.textContent = topFilms.film.title;
-          filmDate.textContent = topFilms.film.release_date;
-          filmOverview.textContent = topFilms.film.overview;
-
-          console.log(topFilms);
+      filmObjs.forEach((x, i) => {
+        if (i < 3) {
+          let film = {
+            id: x.id,
+            image: x.poster_path,
+            title: x.title,
+            release_date: x.release_date,
+            overview: x.overview
+          };
+          topFilms.push(film);
         }
-  };
-xhr.open("GET", "https://api.themoviedb.org/3/discover/movie?primary_release_year.lte=2019&api_key=9ec25de3852c136e9679b83b4d5f1ad2&sort_by=popularity.desc", true);
-xhr.send();
+      });
+      console.log("topFilms: ", topFilms);
+      // filmOutput.innerHTML = xhr.responseText;
+      topFilms.forEach((x, i) => {
+        console.log(topFilms[0].title);
+        //create div for each object
+        // create image title, date and overview elements, for each div
+        var newDiv = document.createElement("div");
+        document.getElementById("film-output").appendChild(newDiv);
+        newDiv.className = "film number [i]";
+        console.log([i].image);
+        // const filmImage = document.querySelector("#film-image");
+        const filmTitle = document.createTextNode("");
+        filmTitle.textContent = topFilms[i].title;
+        const filmDate = document.createTextNode("");
+        filmDate.textContent = topFilms[i].release_date;
+        const filmOverview = document.createTextNode("");
+        filmOverview.textContent = topFilms[i].overview;
 
+        newDiv.appendChild(filmTitle);
+        newDiv.appendChild(filmDate);
+        newDiv.appendChild(filmOverview);
+
+        //
+        //
+
+        console.log(topFilms);
+      });
+    }
+  };
+  xhr.open(
+    "GET",
+    "https://api.themoviedb.org/3/discover/movie?primary_release_year.lte=2019&api_key=9ec25de3852c136e9679b83b4d5f1ad2&sort_by=popularity.desc",
+    true
+  );
+  xhr.send();
 })();
