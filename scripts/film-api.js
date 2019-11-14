a69cf68283d8b989c248cd24ff7ccb8675b6268b
Alexandra O'Mahony <alexandra.omahony1@gmail.com>
2019-11-14 15:13:39 +0000

    added button submission to film-api.js file and tested on live server -it lives
    co-authored-by: Dan <dangoss@posteo.net>

--- scripts/film-api.js
+++ scripts/film-api.js
@@ -1,37 +1,38 @@
-// export let topFilms = [];
-
 let topFilms = [];
 const filmsSection = document.querySelector(".films");
 
-const xhr = new XMLHttpRequest();
-const birthday = document.querySelector(".input-date");
-const url = "https://api.themoviedb.org/3/discover/movie?";
-const api_key = "9ec25de3852c136e9679b83b4d5f1ad2";
-const year = 2019;
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    const filmObjs = JSON.parse(xhr.responseText).results;
-    filmObjs.forEach((x, i) => {
-      if (i < 3) {
-        let film = {
-          id: x.id,
-          image: x.poster_path,
-          title: x.title,
-          release_date: x.release_date,
-          overview: x.overview
-        };
-        topFilms.push(film);
-      }
-    });
-    injectFilms(topFilms);
-  }
-};
-xhr.open(
-  "GET",
-  "https://api.themoviedb.org/3/discover/movie?primary_release_year.lte=2019&api_key=9ec25de3852c136e9679b83b4d5f1ad2&sort_by=popularity.desc",
-  true
-);
-xhr.send();
+submitBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  const xhr = new XMLHttpRequest();
+  let myFilmKey = "9ec25de3852c136e9679b83b4d5f1ad2";
+  let year = document.querySelector(".input-date").value;
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      const filmObjs = JSON.parse(xhr.responseText).results;
+      filmObjs.forEach((x, i) => {
+        if (i < 3) {
+          let film = {
+            id: x.id,
+            image: x.poster_path,
+            title: x.title,
+            release_date: x.release_date,
+            overview: x.overview
+          };
+          topFilms.push(film);
+        }
+      });
+      injectFilms(topFilms);
+    }
+  };
+  xhr.open(
+    "GET",
+    `https://api.themoviedb.org/3/discover/movie?primary_release_year=${year}&api_key=${myFilmKey}`,
+    true
+  );
+  xhr.send();
+});
 
 const injectFilms = function(filmArray) {
   filmArray.forEach((x, i) => {
--- scripts/music-api.js
+++ scripts/music-api.js
@@ -1,15 +1,15 @@
-// let year = 1900;
-const submitBtn = document.querySelector('.submit-date');
-console.log("This is the submit button:", submitBtn)
+// declare btn variable only once in this script, which must run first!
+const submitBtn = document.querySelector(".submit-date");
+console.log("This is the submit button:", submitBtn);
 
-submitBtn.addEventListener('click', function (event) {
+submitBtn.addEventListener("click", function(event) {
   event.preventDefault();
 
   let xhr = new XMLHttpRequest();
-  let year = document.querySelector('.input-date').value; //currently hardcoded to 1983
+  let year = document.querySelector(".input-date").value;
   let url = `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?f_track_release_group_first_release_date_min=${year}0101&f_track_release_group_first_release_date_max=${year}1231&s_track_rating=desc&apikey=7e97ca6319d9e759ef7bcde83951e849`;
 
-  xhr.onload = function () {
+  xhr.onload = function() {
     if (xhr.status == 200) {
       let songs = JSON.parse(xhr.responseText).message.body.track_list;
       let topTracks = [];
@@ -32,6 +32,3 @@ submitBtn.addEventListener('click', function (event) {
   xhr.open("GET", url, true);
   xhr.send();
 });
-
-
-
