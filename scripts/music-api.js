6c0fa01a58913f7b0c0e640c021b9988285d6922
nikkesan <nikke.san@gmail.com>
2019-11-14 15:03:18 +0000

    Put API keys into config files for security
    Relates #16

--- scripts/music-api.js
+++ scripts/music-api.js
@@ -1,9 +1,11 @@
 // let year = 1900;
+const myMusicKey = config.MY_MUSIC_KEY;
+
 let year = document.querySelector('.input-date').value;
 const submitBtn = document.querySelector('.submit-date');
 
 let xhr = new XMLHttpRequest();
-let url = `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?f_track_release_group_first_release_date_min=${year}0101&f_track_release_group_first_release_date_max=${year}1231&s_track_rating=desc&apikey=7e97ca6319d9e759ef7bcde83951e849`;
+let url = `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?f_track_release_group_first_release_date_min=${year}0101&f_track_release_group_first_release_date_max=${year}1231&s_track_rating=desc&apikey=${myMusicKey}`;
 
 submitBtn.onclick = function () {
   xhr.onload = function () {
